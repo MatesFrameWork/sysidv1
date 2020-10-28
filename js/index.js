4b3c7920419d6aba569ec7c0b31d295b097098f3
MatesFrameWork <72851979+MatesFrameWork@users.noreply.github.com>
2020-10-28 15:41:49 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
-// // Get the modal
-// var emailModal = document.getElementById("email-modal");
-//
-// // Get the button that opens the modal
-// var emailModalButton = document.getElementById("email-modal-btn");
-//
-// // Get the <span> element that closes the modal
-// var closeEmailModalButton = document.getElementsByClassName("close-email-modal")[0];
-//
-// // When the user clicks on the button, open the modal
-// emailModalButton.onclick = function() {
-//   emailModal.style.display = "block";
-// }
-//
-// // When the user clicks on <span> (x), close the modal
-// closeEmailModalButton.onclick = function() {
-//   emailModal.style.display = "none";
-// }
-//
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == emailModal) {
-//     emailModal.style.display = "none";
-//   }
-// }
+// Get the modal
+var emailModal = document.getElementById("email-modal");
+
+// Get the button that opens the modal
+var emailModalButton = document.getElementById("email-modal-btn");
+
+// Get the <span> element that closes the modal
+var closeEmailModalButton = document.getElementsByClassName("close-email-modal")[0];
+
+// When the user clicks on the button, open the modal
+emailModalButton.onclick = function() {
+  emailModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeEmailModalButton.onclick = function() {
+  emailModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == emailModal) {
+    emailModal.style.display = "none";
+  }
+}
 
 $(".container-mail-connect-text").click(function(){
   $(".container-mail-connect-text a")[0].click();
@@ -33,8 +33,6 @@ function sendMessage(){
   var clientName = $("#client-name").val();
   var clientEmail = $("#client-email").val();
   var clientMessage = $("#client-message").val();
-
-  alert(clientMessage);
 }
 
 $(document).ready(function(){
@@ -57,4 +55,61 @@ $(document).scroll(function(){
                 $(sectionClass).css({"color": "rgba(54,179,165,1)"});
             };
         });
+});
+
+function navigationIcon() {
+  var x = $("#navigation-menu");
+  if (x.className === "container-navigation") {
+    x.className += " responsive";
+  } else {
+    x.className = "container-navigation";
+  }
+}
+
+$(window).resize(function(){
+  var windowWidth = $(this).width();
+  if (windowWidth<=750){
+    $(".container-title-img .title-img").attr("src","images/mobile_home_image.png");
+    $(".container-tictac-img .tictac-img").attr("src","images/mobile_tictac.png");
+    $(".container-bulb-img .bulb-img").attr("src","images/mobile_bulb.png");
+    $(".container-wheel-img .wheel-img").attr("src","images/mobile_wheel.png");
+    $(".container-pencil-img .pencil-img").attr("src","images/mobile_pencil.png");
+    $(".container-about-img .about-img").attr("src","images/mobile_about_image.png");
+    $(".container-solutions-img .solutions-img").attr("src","images/mobile_solutions_image.png");
+    $(".container-collab-img .collab-img").attr("src","images/mobile_collab_image.png");
+  }
+  else {
+    $(".container-title-img .title-img").attr("src","images/home_image.png");
+    $(".container-tictac-img .tictac-img").attr("src","images/tictac.png");
+    $(".container-bulb-img .bulb-img").attr("src","images/bulb.png");
+    $(".container-wheel-img .wheel-img").attr("src","images/wheel.png");
+    $(".container-pencil-img .pencil-img").attr("src","images/pencil.png");
+    $(".container-about-img .about-img").attr("src","images/about_image.png");
+    $(".container-solutions-img .solutions-img").attr("src","images/solutions_image.png");
+    $(".container-collab-img .collab-img").attr("src","images/collab_image.png");
+  }
+});
+
+$(document).ready(function(){
+  var windowWidth = $(this).width();
+  if (windowWidth<=750){
+    $(".container-title-img .title-img").attr("src","images/mobile_home_image.png");
+    $(".container-tictac-img .tictac-img").attr("src","images/mobile_tictac.png");
+    $(".container-bulb-img .bulb-img").attr("src","images/mobile_bulb.png");
+    $(".container-wheel-img .wheel-img").attr("src","images/mobile_wheel.png");
+    $(".container-pencil-img .pencil-img").attr("src","images/mobile_pencil.png");
+    $(".container-about-img .about-img").attr("src","images/mobile_about_image.png");
+    $(".container-solutions-img .solutions-img").attr("src","images/mobile_solutions_image.png");
+    $(".container-collab-img .collab-img").attr("src","images/mobile_collab_image.png");
+  }
+  else {
+    $(".container-title-img .title-img").attr("src","images/home_image.png");
+    $(".container-tictac-img .tictac-img").attr("src","images/tictac.png");
+    $(".container-bulb-img .bulb-img").attr("src","images/bulb.png");
+    $(".container-wheel-img .wheel-img").attr("src","images/wheel.png");
+    $(".container-pencil-img .pencil-img").attr("src","images/pencil.png");
+    $(".container-about-img .about-img").attr("src","images/about_image.png");
+    $(".container-solutions-img .solutions-img").attr("src","images/solutions_image.png");
+    $(".container-collab-img .collab-img").attr("src","images/collab_image.png");
+  }
 });
